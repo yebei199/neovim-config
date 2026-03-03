6901876647ac231ca2b45b7ba34c41c7ac5e0c40
Sibyl <scauyb@qq.com>
2026-03-03 16:46:24 +0800

    change to nixfmt

--- lua/config/langs/nix.lua
+++ lua/config/langs/nix.lua
@@ -1,23 +1,23 @@
 return {
-  formatter = "alejandra", -- Nix 社区推荐的格式化器
-  lsp = {
-    nixd = {
-      -- nixd 的配置项
-      settings = {
+    formatter = "nixfmt", -- Nix 社区推荐的格式化器
+    lsp = {
         nixd = {
-          nixpkgs = {
-            expr = "import <nixpkgs> {}", -- 可选: 指定 nixpkgs 表达式
-          },
-          options = {
-            nixos = {
-              expr = "(import <nixpkgs/nixos> {}).options",
+            -- nixd 的配置项
+            settings = {
+                nixd = {
+                    nixpkgs = {
+                        expr = "import <nixpkgs> {}", -- 可选: 指定 nixpkgs 表达式
+                    },
+                    options = {
+                        nixos = {
+                            expr = "(import <nixpkgs/nixos> {}).options",
+                        },
+                        home_manager = {
+                            expr = "(import <home-manager> {}).options",
+                        },
+                    },
+                },
             },
-            home_manager = {
-              expr = "(import <home-manager> {}).options",
-            },
-          },
         },
-      },
     },
-  },
 }
