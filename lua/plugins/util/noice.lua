f765bfad89f513394cbc40508b53c0734aad3d35
Sibyl <scauyb@qq.com>
2026-03-04 10:30:05 +0800

    fix: disable automatic window splits on project open
    - noice: set long_message_to_split = false to prevent LSP/plugin notifications from auto-splitting
    - oil: set default_file_explorer = false to disable auto-opening on directory entry
    
    Fixes issue where empty split windows appeared automatically when opening projects.

--- lua/plugins/util/noice.lua
+++ lua/plugins/util/noice.lua
@@ -14,7 +14,7 @@ return {
     presets = {
       bottom_search = true,
       command_palette = true,
-      long_message_to_split = true,
+      long_message_to_split = false,
     },
   },
   dependencies = {
