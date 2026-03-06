031bc002ba7e2eab97b76cb102a16f54a9092d29
Sibyl <scauyb@qq.com>
2026-03-06 13:47:02 +0800

    feat(keymaps): add motion keybindings for line and file navigation
    Add three new keybindings for quick navigation:
    - gh: jump to line start (equivalent to ^)
    - gl: jump to line end (equivalent to $)
    - ge: jump to file end (equivalent to G)
    
    Updates keymap documentation to reflect the new keybindings.

--- lua/config/keymaps/init.lua
+++ lua/config/keymaps/init.lua
@@ -41,6 +41,11 @@ map("t", "<esc><esc>", "<cmd>stopinsert<CR>", { desc = "which_key_ignore" })
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
+-- Motion: line start, line end, file end
+map("n", "gh", "^", { desc = "Go to Line Start" })
+map("n", "gl", "$", { desc = "Go to Line End" })
+map("n", "ge", "G", { desc = "Go to File End" })
+
 -- Move Lines
 map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
